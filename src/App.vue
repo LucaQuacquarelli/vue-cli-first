49dbeae1e14c733c4f8cd52077fe177d006c444d
LucaQuacquarelli <lucaq82@libero.it>
2021-05-28 17:03:34 +0200

    Aggiunto componente Hero (Jumbotron)

--- src/App.vue
+++ src/App.vue
@@ -3,29 +3,44 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <Title/>
+    <Hero/>
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
-import Title from './components/Title.vue'
+import Title from './components/Title'
+import Hero from './components/Hero'
 
 export default {
   name: 'App',
   components: {
     // HelloWorld
-    Title
+    Title,
+    Hero
   }
 }
 </script>
 
 <style lang="scss">
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
 #app {
+  width: 100%;
+  height: 100vh;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: #15243c;
+  color: white;
+}
+
+img {
+  width: 100px;
 }
 </style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="jbt">
+        <h2>The Progressive JavaScript Framework</h2>
+        <button class="start">Get Started</button>
+        <button class="hub">github</button>
+    </div>
+</template>
+
+<style scoped lang="css">
+    .jbt {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: calc(100vh - 150px);
+        padding: 50px;
+        border-radius: 15px;
+        background-image: url(https://wallpapercave.com/wp/wp7134546.jpg);
+        background-size: cover;
+        background-position: center;
+        text-align: left;
+    }
+    .jbt button {
+        margin: 20px 10px;
+        padding: 10px 40px;
+        border-radius: 30px;
+        font-size: 15px;
+        text-transform: uppercase;
+        cursor: pointer;
+    }
+
+    .jbt .start {
+        background-color: white;
+        border: 1px solid #42b983;
+        color: #42b983;
+    }
+
+    .jbt .hub {
+        background-color: lightgray;
+        border: 1px solid grey;
+        color: grey;
+    }
+</style>
